8a54cfc2c4643dca923783218f1d299e60e14d99
Antônio Pasquali Coelho <141248303+AntonioPCoelho@users.noreply.github.com>
2023-11-11 21:48:36 -0300

    Update Assinatura.java

--- src/Assinatura.java
+++ src/Assinatura.java
@@ -1,9 +1,49 @@
-public class Assinatura extends Aplicativo {
-    
+public class Assinatura {
+    private String codigo, codigoApp, cpfCliente;
+    private int inicioMes, inicioAno, encerramentoMes, encerramentoAno;
 
-    int codigoAssinatura;
+    public Assinatura(String codigo, String codigoApp, String cpfCliente, int inicioMes, int inicioAno, int encerramentoMes, int encerramentoAno) {
+        this.codigo = codigo;
+        this.codigoApp = codigoApp;
+        this.cpfCliente = cpfCliente;
+        this.inicioMes = inicioMes;
+        this.inicioAno = inicioAno;
+        this.encerramentoMes = encerramentoMes;
+        this.encerramentoAno = encerramentoAno;
+    }
 
-    public Assinatura(int codigo, String nome, double preco, Aplicativo.SO so) {
-        super(codigo, nome, preco, so);
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getCodigoAplicativo() {
+        return codigoApp;
+    }
+
+    public String getCpfCliente() {
+        return cpfCliente;
+    }
+
+    public int getInicioMes() {
+        return inicioMes;
+    }
+
+    public int getInicioAno() {
+        return inicioAno;
+    }
+
+    public int getEncerramentoMes() {
+        return encerramentoMes;
+    }
+
+    public int getEncerramentoAno() {
+        return encerramentoAno;
+    }
+
+    @Override
+    public String toString() {
+        return "Assinatura [codigo=" + codigo + ", codigoApp=" + codigoApp + ", cpfCliente=" + cpfCliente
+                + ", inicioMes=" + inicioMes + ", inicioAno=" + inicioAno + ", encerramentoMes=" + encerramentoMes
+                + ", encerramentoAno=" + encerramentoAno + "]";
     }
 }
