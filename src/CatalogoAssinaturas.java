621e518f9f2f895d093037af1e925da3dc8e1518
Antônio Pasquali Coelho <141248303+AntonioPCoelho@users.noreply.github.com>
2023-11-13 14:58:28 -0300

    update

---
+++ src/CatalogoAssinaturas.java
@@ -0,0 +1,59 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class CatalogoAssinaturas {
+    private List<Assinatura> assinaturas;
+
+    public CatalogoAssinaturas() {
+        assinaturas = new ArrayList<>();
+    }
+
+    public void cadastra(Assinatura s) {
+        assinaturas.add(s);
+    }
+
+    public void remove(Assinatura s) {
+        assinaturas.remove(s);
+    }
+
+    public Assinatura getAssinaturaNaLinha(int linha) {
+        if (linha >= assinaturas.size()) {
+            return null;
+        }
+        return assinaturas.get(linha);
+    }
+
+    public int getQtdade() {
+        return assinaturas.size();
+    }
+
+    public Stream<Assinatura> getStream() {
+        return assinaturas.stream();
+    }
+
+    public void loadFromFile() {
+        Path assinaturasFilePath = Path.of("assinaturas.dat");
+        try (Stream<String> appsStream = Files.lines(assinaturasFilePath)) {
+            appsStream.forEach(str -> assinaturas.add(Assinatura.fromLineFile(str)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void saveToFile() {
+        Path appsFilePath = Path.of("apps.dat");
+        try (PrintWriter writer = new PrintWriter(Files.newBufferedWriter(appsFilePath, StandardCharsets.UTF_8))) {
+            for (Assinatura s : assinaturas) {
+                writer.println(s.toLineFile());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
