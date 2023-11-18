e1c10abdd13f4ac652f64c59f90cc6679134b800
Antônio Pasquali Coelho <141248303+AntonioPCoelho@users.noreply.github.com>
2023-11-18 16:08:26 -0300

    Update CatalogoAssinaturas.java

--- src/CatalogoAssinaturas.java
+++ src/CatalogoAssinaturas.java
@@ -3,8 +3,10 @@ import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class CatalogoAssinaturas {
@@ -20,6 +22,39 @@ public class CatalogoAssinaturas {
 
     public void remove(Assinatura s) {
         assinaturas.remove(s);
+    }
+
+    public void gerenciarAssinatura(Assinatura s) {
+        if (assinaturaAtiva(s)) {
+            cancelaAssinatura(s);
+        } else {
+            cadastra(s);
+        }
+    }
+
+    private boolean assinaturaAtiva(Assinatura s) {
+        List<Assinatura> sAtiva = assinaturas.stream()
+            .filter(a -> a.getCpfCliente().equals(s.getCpfCliente()) &&
+                    a.getCodigoApp() == s.getCodigoApp() &&
+                    a.getFim().equals("00/00"))
+            .collect(Collectors.toList());  
+        return !sAtiva.isEmpty();
+    }
+
+    private void cancelaAssinatura(Assinatura s) {
+        for (Assinatura a : assinaturas) {
+            if (a.getCpfCliente().equals(s.getCpfCliente()) &&
+                a.getCodigoApp() == s.getCodigoApp() &&
+                a.getFim().equals("00/00")) {
+                    a.setFim(getData());
+                    break;
+                }
+        }
+    }
+
+    private String getData() {
+        LocalDate dataAtual = LocalDate.now();
+        return String.format("%02d/%02d", dataAtual.getDayOfMonth(), dataAtual.getMonthValue());
     }
 
     public Assinatura getAssinaturaNaLinha(int linha) {
