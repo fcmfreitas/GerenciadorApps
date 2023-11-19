488989b0bfb190944139488d917be92de9f94bb7
Francisco Freitas <136604423+fcmfreitas@users.noreply.github.com>
2023-11-19 17:39:44 -0300

    Update Assinatura.java

--- src/Assinatura.java
+++ src/Assinatura.java
@@ -1,66 +1,64 @@
-public class Assinatura {
-    private int codigo, codigoApp;
-    private String cpfCliente, inicio, fim;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
-    public Assinatura(int codigo, int codigoApp, String cpfCliente, String inicio, String fim) {
-        this.codigo = codigo;
-        this.codigoApp = codigoApp;
-        this.cpfCliente = cpfCliente;
-        this.inicio = inicio;
-        this.fim = fim;
+public class CatalogoAssinaturas {
+    private List<Assinatura> assinaturas;
+
+    public CatalogoAssinaturas() {
+        assinaturas = new ArrayList<>();
     }
 
-    public int getCodigo() {
-        return codigo;
+    public void cadastra(Assinatura s) {
+        assinaturas.add(s);
     }
 
-    public void setCodigo(int codigo) {
-        this.codigo = codigo;
+    public void remove(Assinatura s) {
+        assinaturas.remove(s);
     }
 
-    public int getCodigoApp() {
-        return codigoApp;
+    public void removeAssinatura(int codigo){
+        assinaturas.removeIf(assinatura -> codigo == assinatura.getCodigo());
+        saveToFile();
     }
 
-    public void setCodigoApp(int codigoApp) {
-        this.codigoApp = codigoApp;
+    public Assinatura getAssinaturaNaLinha(int linha) {
+        if (linha >= assinaturas.size()) {
+            return null;
+        }
+        return assinaturas.get(linha);
     }
 
-    public String getCpfCliente() {
-        return cpfCliente;
+    public int getQtdade() {
+        return assinaturas.size();
     }
 
-    public void setCpfCliente(String cpfCliente) {
-        this.cpfCliente = cpfCliente;
+    public Stream<Assinatura> getStream() {
+        return assinaturas.stream();
     }
 
-    public String getInicio() {
-        return inicio;
+    public void loadFromFile() {
+        Path assinaturasFilePath = Path.of("assinaturas.dat");
+        try (Stream<String> appsStream = Files.lines(assinaturasFilePath)) {
+            appsStream.forEach(str -> assinaturas.add(Assinatura.fromLineFile(str)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void setInicio(String inicio) {
-        this.inicio = inicio;
-    }
- 
-    public String getFim() {
-        return fim;
-    }
-
-    public void setFim(String fim) {
-        this.fim = fim;
-    }
-
-    public String toLineFile(){
-        return codigo+","+codigoApp+","+cpfCliente+","+inicio+","+fim;
-    }
-
-    public static Assinatura fromLineFile(String line){
-        String[] tokens = line.split(",");
-        int codigo = Integer.parseInt(tokens[0]);
-        int codigoApp = Integer.parseInt(tokens[1]);
-        String cpfCliente = tokens[2];
-        String inicio = tokens[3];
-        String fim = tokens[4];
-        return new Assinatura(codigo,codigoApp,cpfCliente,inicio,fim);
+    public void saveToFile() {
+        Path assinaturasFilePath = Path.of("assinaturas.dat");
+        try (PrintWriter writer = new PrintWriter(Files.newBufferedWriter(assinaturasFilePath, StandardCharsets.UTF_8))) {
+            for (Assinatura s : assinaturas) {
+                writer.println(s.toLineFile());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
